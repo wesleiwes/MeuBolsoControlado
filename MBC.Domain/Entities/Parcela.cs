800f704df0b198b2980b1bba649eb9b628cf46d6
Weslei Rodrigues Alves <wesleiralves@gmail.com>
2025-02-10 22:54:37 -0300

    Initial commit com as entidades e estrutura básica do projeto

---
+++ MBC.Domain/Entities/Parcela.cs
@@ -0,0 +1,12 @@
+﻿namespace MBC.Domain.Entities;
+public class Parcela
+{
+    public int Id { get; set; }
+    public int Numero { get; set; }
+    public decimal Valor { get; set; }
+    public DateTime DataVencimento { get; set; }
+    public bool Pago { get; set; }
+    public int DespesaId { get; set; }
+    public Despesa Despesa { get; set; }
+}
+
