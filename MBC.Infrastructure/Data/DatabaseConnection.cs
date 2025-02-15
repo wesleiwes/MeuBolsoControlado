a545d335ee233d973c488c7c492e2570e97ea4d9
Weslei Rodrigues Alves <wesleiralves@gmail.com>
2025-02-15 00:47:54 -0300

    Implementação da conexao de string

--- MBC.Infrastructure/Data/DatabaseConnection.cs
+++ MBC.Infrastructure/Data/DatabaseConnection.cs
@@ -1,4 +1,8 @@
 ﻿namespace MBC.Infrastructure.Data;
 public class DatabaseConnection
 {
+    const string _connectionString =
+           @"Server=DESKTOP-8LAD38N\SQLEXPRESS;Database=MeuBolsoControladoDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+    public static string GetConnectionString() => _connectionString;
 }
