c5ff181dbb9210aec98d0ae81466f6084d386cab
Weslei Rodrigues Alves <wesleiralves@gmail.com>
2025-02-15 00:58:13 -0300

    Implementação da camada de infraestrutura com as classes repository

---
+++ MBC.Infrastructure/Data/Repositories/ParcelaRepository.cs
@@ -0,0 +1,31 @@
+﻿using MBC.Domain.Entities;
+using MBC.Domain.Entities.Base;
+using MBC.Domain.RepositoriesInterface;
+using MBC.Infrastructure.Mappers;
+
+namespace MBC.Infrastructure.Data.Repositories;
+public class ParcelaRepository : IParcelaRepository
+{
+    private readonly ParcelaMapper _parcelaMapper = new();
+
+    public void Adicione(Parcela parcela) => 
+        _parcelaMapper.Adicionar(parcela);    
+
+    public void Atualize(Parcela parcela) => 
+        _parcelaMapper.Atualizar(parcela);
+
+    public Parcela BusquePorId(int id) =>
+        _parcelaMapper.BuscarPorId(id);
+
+    public Parcela BusqueProximaParcelaNaoPaga(int transacaoId) => 
+        _parcelaMapper.BuscarProximaParcelaNaoPaga(transacaoId);
+
+    public decimal CalculeValorRestante(int transacaoId) =>
+        _parcelaMapper.CalcularValorRestante(transacaoId);
+
+    public IEnumerable<Parcela> ListePorTransacao(int transacaoId) =>
+        _parcelaMapper.ListarPorTransacao(transacaoId);
+
+    public void Remova(int id) =>
+        _parcelaMapper.Remover(id);
+}
---
+++ MBC.Infrastructure/Data/Repositories/TransacaoRepository.cs
@@ -0,0 +1,48 @@
+﻿using MBC.Domain.Entities;
+using MBC.Domain.Entities.Base;
+using MBC.Domain.Enums;
+using MBC.Domain.RepositoriesInterface;
+
+namespace MBC.Infrastructure.Data.Repositories;
+public class TransacaoRepository : ITransacaoRepository
+{
+    public void Adicione(IEntidade entidade)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Atualize(IEntidade entidade)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEntidade BusquePorId(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transacao> ListePorCategoria(CategoriaTransacao categoria, int usuarioId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transacao> ListePorPeriodo(DateTime inicio, DateTime fim, int usuarioId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transacao> ListePorUsuario(int usuarioId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal ObtenhaTotalPorPeriodo(DateTime inicio, DateTime fim, int usuarioId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remova(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
